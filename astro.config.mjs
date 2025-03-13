c96392eb8c2266a68d8de7c16e420c2c9ee5e9df
handystudio <panda@handystudio.app>
2025-03-13 16:32:52 +0800

    init version

---
+++ astro.config.mjs
@@ -0,0 +1,30 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+import mdx from '@astrojs/mdx';
+import alpinejs from '@astrojs/alpinejs';
+import preact from '@astrojs/preact';
+import react from '@astrojs/react';
+import solidJs from '@astrojs/solid-js';
+import svelte from '@astrojs/svelte';
+import vue from '@astrojs/vue';
+import partytown from '@astrojs/partytown';
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  integrations: [
+    mdx(),
+    alpinejs(),
+    preact(),
+    react(),
+    solidJs(),
+    svelte(),
+    vue(),
+    partytown()
+  ]
+});
